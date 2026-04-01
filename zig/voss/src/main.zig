ff6e43a82aea1661d9ca151c3cfa84e144a53542
awkward crow <awkward.crow@gmail.com>
2026-04-01 16:45:54 +0100

    two passes over input vector

--- zig/voss/src/main.zig
+++ zig/voss/src/main.zig
@@ -2,50 +2,51 @@ const std = @import("std");
 const voss = @import("voss");
 
 pub fn main() !void {
-    const s = "_EAt my $hortZ..";
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
     const n = 16;
-    var t: @Vector(n, u8) = s.*;
-    std.debug.print("{s}\n", .{@as([n]u8, t)});
 
     const A: @Vector(n, u8) = @splat(@as(u8, 'A'));
     const Z: @Vector(n, u8) = @splat(@as(u8, 'Z'));
-
-    const upper = (A <= t) & (t <= Z);
-    std.debug.print("{any}\n", .{@as([n]bool, upper)});
-
     const d: @Vector(n, u8) = @splat(@as(u8, 0x20));
     const zero: @Vector(n, u8) = @splat(@as(u8, 0));
+    const a: @Vector(n, u8) = @splat(@as(u8, 'a'));
+    const z: @Vector(n, u8) = @splat(@as(u8, 'z'));
+    const indices = std.simd.iota(u8, n);
+    const nulls: @Vector(n, u8) = @splat(@as(u8, n));
 
+    const s = "_EAt my $hortZ..";
+    try stdout.print("s is {s}\n", .{s});
+
+    var t: @Vector(n, u8) = s.*;
+
+    const upper = (A <= t) & (t <= Z);
     t |= @select(u8, upper, d, zero);
-    std.debug.print("{s}\n", .{@as([n]u8, t)});
-    std.debug.print("{s}\n", .{s});
+    const alpha = (a <= t) & (t <= z);
 
-    //     const u = t | @select(u8, upper, d, zero);
-    //
-    //     std.debug.print("{s}\n", .{@as([n]u8, u)});
-    //
-    //     const a: @Vector(n, u8) = @splat(@as(u8, 'a'));
-    //     const z: @Vector(n, u8) = @splat(@as(u8, 'z'));
-    //
-    //     const alpha = (a <= u) & (u <= z);
-    //     std.debug.print("{any}\n", .{@as([n]bool, alpha)});
-    //
-    //     const indices = std.simd.iota(u8, n);
-    //     const nulls: @Vector(n, u8) = @splat(@as(u8, n));
-    //
-    //     const p = @reduce(.Min, @select(u8, alpha, indices, nulls));
-    //     std.debug.print("{d}\n", .{p});
-    //
-    //     const mask: @Vector(n, u8) = @splat(@as(u8, p));
-    //     std.debug.print("{any}\n", .{@as([n]u8, mask)});
-    //
-    //     const rho = @select(u8, (~alpha) & (mask < indices), indices, nulls);
-    //     const q = @reduce(.Min, rho);
-    //     std.debug.print("{any}\n", .{@as([n]u8, rho)});
-    //     std.debug.print("{d}\n", .{q});
-    //
-    //     std.debug.print("{s}\n", .{s[p..q]});
-    //     std.debug.print("{s}\n", .{@as([n]u8, u)[p..q]});
+    var p: u8 = @reduce(.Min, @select(u8, alpha, indices, nulls));
+    try stdout.print("p_0 is {d}\n", .{p});
 
-    std.debug.print("ok\n", .{});
+    var p_v: @Vector(n, u8) = @splat(@as(u8, p));
+    var rho = @select(u8, (~alpha) & (p_v < indices), indices, nulls);
+    var q = @reduce(.Min, rho);
+    try stdout.print("q is {d}\n", .{q});
+
+    try stdout.print("{s}\n", .{@as([n]u8, t)[p..q]});
+
+    const q_v: @Vector(n, u8) = @splat(@as(u8, q));
+    p = @reduce(.Min, @select(u8, alpha & (q_v <= indices), indices, nulls));
+    try stdout.print("p_1 is {d}\n", .{p});
+
+    p_v = @splat(@as(u8, p));
+    rho = @select(u8, (~alpha) & (p_v < indices), indices, nulls);
+    q = @reduce(.Min, rho);
+    try stdout.print("q_1 is {d}\n", .{q});
+
+    try stdout.print("{s}\n", .{@as([n]u8, t)[p..q]});
+
+    try stdout.print("ok\n", .{});
+    try stdout.flush();
 }
